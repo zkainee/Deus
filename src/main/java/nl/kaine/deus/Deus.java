d9dce5e54cfd6f7b029a21b4f5731aa54fa515d0
kaine <kainevommeren@gmail.,com>
2022-07-25 13:27:17 +0200

    Entity manipulation

--- src/main/java/nl/kaine/deus/Deus.java
+++ src/main/java/nl/kaine/deus/Deus.java
@@ -1,6 +1,10 @@
 package nl.kaine.deus;
 
-import nl.kaine.deus.commands.Config;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -8,10 +12,12 @@ import java.util.Objects;
 public final class Deus extends JavaPlugin {
     @Override
     public void onEnable() {
-        getConfig().options().copyDefaults();
-        saveDefaultConfig();
-
-        Objects.requireNonNull(getCommand("config")).setExecutor(new Config(this));
         System.out.println("DEUS | LOADED");
+
+        Entity polarBear = Objects.requireNonNull(Bukkit.getWorld("world")).spawnEntity(new Location(Bukkit.getWorld("world"), -60, 64, -99), EntityType.POLAR_BEAR);
+        polarBear.setGlowing(true);
+
+        ArmorStand stand = (ArmorStand) Objects.requireNonNull(Bukkit.getWorld("world")).spawnEntity(new Location(Bukkit.getWorld("world"), -63, 64, -99), EntityType.ARMOR_STAND);
     }
+
 }
